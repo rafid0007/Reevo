f62fbf670748a5a0c288dce7121991c4a03c53a5
arko9858 <arko9858@gmail.com>
2021-04-08 23:28:44 +0600

    Added Cart functionality

--- components/layout/cartItem/cartItem.jsx
+++ components/layout/cartItem/cartItem.jsx
@@ -1,34 +1,46 @@
 import React from "react";
-
-import { Paper } from "@material-ui/core";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import DeleteIcon from "@material-ui/icons/Delete";
-
+import IconButton from "@material-ui/core/IconButton";
 import cartItemStyles from "./cartItem.module.scss";
+import { Typography } from "@material-ui/core";
 
-
-
-const CartItem = ({productImg,productName,productPriceRate,productQuantity}) => {
+const CartItem = ({
+  itemId,
+  productImg,
+  productName,
+  productPriceRate,
+  productQuantity,
+  incrementQuantity,
+  decrementQuantity,
+  deleteItem,
+}) => {
   return (
     <div className={cartItemStyles.cartItem}>
       <div className={cartItemStyles.imgContainer}>
-        <img src={"https://i.ibb.co/zscCfcm/black-tee-1.png"} alt="tee shirt" />
+        <img src={productImg} alt={productName} />
       </div>
       <div className={cartItemStyles.cartPriceAndTitle}>
-        <h5>Black Tee shirt with 100% fiber with cotton</h5>
-        <h6>৳ 500.00</h6>
+        <h5>{productName}</h5>
+        <h6>৳ {productPriceRate}.00</h6>
       </div>
       <div className={cartItemStyles.quantity}>
-        <ArrowBackIosIcon style={{ fontSize: "1.6rem", cursor: "pointer" }} />
-        <span>X 1</span>
-        <ArrowForwardIosIcon
-          style={{ fontSize: "1.6rem", cursor: "pointer" }}
-        />
+        <IconButton onClick={() => decrementQuantity(itemId)}>
+          <ArrowBackIosIcon style={{ fontSize: "1.6rem", cursor: "pointer" }} />
+        </IconButton>
+        <Typography>{productQuantity}</Typography>
+        <IconButton onClick={() => incrementQuantity(itemId)}>
+          <ArrowForwardIosIcon
+            style={{ fontSize: "1.6rem", cursor: "pointer" }}
+          />
+        </IconButton>
       </div>
       <div className={cartItemStyles.totalPrice}>
-        <h4>৳ 500.00</h4>
-        <DeleteIcon style={{ fontSize: "2rem", cursor: "pointer" }} />
+        <h4>৳ {productPriceRate * productQuantity}.00</h4>
+        <IconButton onClick={() => deleteItem(itemId)}>
+          <DeleteIcon style={{ fontSize: "2rem", cursor: "pointer" }} />
+        </IconButton>
       </div>
     </div>
   );
